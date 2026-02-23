4df316f339908c21ff8200201a54861b1b48700f
sakhi-shraddha-sst <shraddha.st.qa@gmail.com>
2026-02-23 18:15:23 +0530

    23rd Feb

--- src/test/java/org/som/TestSelenium07_Selenium_Commands.java
+++ src/test/java/org/som/TestSelenium07_Selenium_Commands.java
@@ -1,7 +1,18 @@
 package org.som;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
 public class TestSelenium07_Selenium_Commands {
     public static void main(String[] args) {
 
+        WebDriver driver = new SafariDriver();
+        driver.get("https://katalon-demo-cura.herokuapp.com/");
+
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+        System.out.println(driver.getPageSource());
+
+        driver.quit();
     }
 }
